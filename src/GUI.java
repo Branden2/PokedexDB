3bac02cba2c7b103e8ad75c33d8eb0e5d938bc11
bpatterson98 <brandonpatterson100@hotmail.com>
2018-03-23 19:14:38 -0300

    Add files via upload

--- src/GUI.java
+++ src/GUI.java
@@ -11,15 +11,20 @@ import javafx.scene.*;
 import javafx.scene.effect.*;
 import javafx.scene.media.Media;
 import java.io.File;
+import java.net.URL;
 
 
 public class GUI extends Application{
 
     private Stage window;
     private Scene mainMenu;
-    private Scene pkmnMenu;
-    private Scene regionMenu;
-    private Scene userMenu;
+    private RgnMenu rgn;
+    private PkmnMenu pkmn;
+    private UserMenu user;
+    private Button pkmnMenuButton;
+    private Button userMenuButton;
+    private Button rgnMenuButton;
+    private Button exitButton;
     //private MediaPlayer mainMedia = new MediaPlayer(new Media(new File("oaksResearchLabMusic.mp3").toURI().toString()));
 
 
@@ -29,70 +34,100 @@ public class GUI extends Application{
 
     @Override
     public void start(Stage window) {
-        
+
+        URL oakResource = getClass().getResource("oakResearchLabMusic.mp3");
+        Media oakMedia = new Media (oakResource.toString());
+        MediaPlayer oakPlayer = new MediaPlayer(oakMedia);
+        oakPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+
+        URL menuResource = getClass().getResource("menuSelect.mp3");
+        Media menuMedia = new Media (menuResource.toString());
+        MediaPlayer menuPlayer = new MediaPlayer(menuMedia);
+
+        oakPlayer.play();
+
+        //import out images
         Image oak = new Image("oakBackground.jpg");
         Image pokeball = new Image("pokeball.png");
-        Image pkmnImg = new Image("PkmnMenu.jpg");
 
+        //make our background for main menu
         BackgroundSize size = new BackgroundSize(100,100,true,true,true,false);
-        BackgroundImage pkmnBackground = new BackgroundImage(pkmnImg, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size);
         BackgroundImage oakBackground = new BackgroundImage(oak, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size);
         Background mainBackground = new Background(oakBackground);
-        Background pkmnMenuBackground = new Background(pkmnBackground);
 
+        //setting up window basics
         window.setTitle("Professor Oak's Pokemon Database");
         window.getIcons().add(pokeball);
 
+        //set up buttons w/ text
+        pkmnMenuButton = new Button("Pokemon");
+        userMenuButton = new Button("Users");
+        rgnMenuButton = new Button("Regions");
+        exitButton = new Button("Exit");
 
-        Button pkmnMenuButton = new Button("Pokemon");
-        Button userMenuButton = new Button("Users");
-        Button rgnMenuButton = new Button("Regions");
-        Button exitButton = new Button("Exit");
-
-
+        //style all of the buttons
         styleButton(pkmnMenuButton);
         styleButton(userMenuButton);
         styleButton(rgnMenuButton);
         styleButton(exitButton);
 
-
         BorderPane borderMain = new BorderPane();
-        BorderPane borderPkmn = new BorderPane();
 
-        VBox leftButtons = new VBox();
-        VBox rightButtons = new VBox();
+        VBox leftButtons = new VBox(pkmnMenuButton,userMenuButton);
+        VBox rightButtons = new VBox(rgnMenuButton,exitButton);
 
+        //spacing vbox buttons
         rightButtons.setSpacing(100);
         leftButtons.setSpacing(100);
         rightButtons.setPadding(new Insets(300,225,50,100));
         leftButtons.setPadding(new Insets(300,100,50,225));
 
-
+        //adding panels to borderpane
         borderMain.setLeft(leftButtons);
         borderMain.setRight(rightButtons);
         borderMain.setBackground(mainBackground);
 
-
-        leftButtons.getChildren().add(pkmnMenuButton);
-        leftButtons.getChildren().add(userMenuButton);
-        rightButtons.getChildren().add(rgnMenuButton);
-        rightButtons.getChildren().add(exitButton);
-
-
+        //making new scene and setting it to window
         mainMenu = new Scene(borderMain,1024,576);
         window.setScene(mainMenu);
 
-        pkmnMenu = new Scene(borderPkmn, 1024, 576);
-        borderPkmn.setBackground(pkmnMenuBackground);
+        pkmn = new PkmnMenu(window, mainMenu);
 
-        pkmnMenuButton.setOnTouchReleased( e -> {
-            window.setScene(pkmnMenu);
+        pkmnMenuButton.setOnMousePressed( e -> {
+            menuPlayer.play();
+        });
+
+        pkmnMenuButton.setOnMouseReleased( e -> {
+            window.setScene(pkmn.getScene());
+            menuPlayer.stop();
 
         });
 
+        rgn = new RgnMenu(window, mainMenu);
+
+        rgnMenuButton.setOnMousePressed( e -> {
+            menuPlayer.play();
+        });
+
+        rgnMenuButton.setOnMouseReleased( e -> {
+            window.setScene(rgn.getScene());
+            menuPlayer.stop();
+        });
+
+        user = new UserMenu(window, mainMenu);
+
+        userMenuButton.setOnMousePressed(e -> {
+            menuPlayer.play();
+        });
+
+        userMenuButton.setOnMouseReleased(e -> {
+            window.setScene(user.getScene());
+            oakPlayer.play();
+        });
+
         exitButton.setOnMouseReleased(e -> {
-            //mainMedia.stop();
             System.exit(0);
+            oakPlayer.stop();
         });
 
         //mainMedia.play();
